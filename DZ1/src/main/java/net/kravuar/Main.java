294b78372e9194a92f9cb8a3cfc763a30737b531
kravuar <steam_webmoney@mail.ru>
2023-12-24 12:36:14 +0300

    reinit

---
+++ DZ1/src/main/java/net/kravuar/Main.java
@@ -0,0 +1,9 @@
+package net.kravuar;
+
+public class Main {
+    public static void main(String[] args) {
+        var bebe = new Child();
+        System.out.println("=============");
+        var ivan = new Child("Ivan");
+    }
+}
---
+++ DZ2/src/main/java/net/kravuar/Main.java
@@ -0,0 +1,61 @@
+package net.kravuar;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        var words = Arrays.asList(
+                "w1", "w2", "w3", "w4",
+                "w1", "w1", "w3", "w5",
+                "w6", "w2", "w7", "w8"
+        );
+
+        System.out.println("DATA:");
+        System.out.println(words);
+
+        System.out.println("DISTINCT:");
+        System.out.println(new HashSet<>(words));
+
+        System.out.println("COUNTS:");
+        System.out.println(words.parallelStream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()
+                ))
+        );
+
+        System.out.println();
+        System.out.println("================================");
+        System.out.println();
+
+        var phoneBook = new PhoneBook();
+        phoneBook.add("Бебебе", "1");
+        phoneBook.add("Бабаба", "2");
+        phoneBook.add("Бубубу", "3");
+        phoneBook.add("Быбыбы", "4");
+        phoneBook.add("Бабаба", "5");
+        phoneBook.add("Бибиби", "6");
+        phoneBook.add("Бебебе", "7");
+        phoneBook.add("Бъбъбъ", "8");
+        phoneBook.add("Бебебе", "9");
+        phoneBook.add("Бъбъбъ", "10");
+
+        var byBebebe = phoneBook.get("Бебебе");
+        assert (byBebebe.size() == 3);
+        System.out.println("Бебебе");
+        System.out.println(byBebebe);
+
+        var byBababa = phoneBook.get("Бабаба");
+        assert (byBebebe.size() == 2);
+        System.out.println("Бабаба");
+        System.out.println(byBababa);
+
+        var byBibibi = phoneBook.get("Бибиби");
+        assert (byBebebe.size() == 1);
+        System.out.println("Бибиби");
+        System.out.println(byBibibi);
+    }
+}
