116980bdbeb6d4a750e911de201e88edb931eb9d
kravuar <steam_webmoney@mail.ru>
2024-01-13 12:40:43 +0300

    -> parametrized test

--- DZ5/src/test/java/net/kravuar/components/subjects/calculator/CalculatorTests.java
+++ DZ5/src/test/java/net/kravuar/components/subjects/calculator/CalculatorTests.java
@@ -1,6 +1,8 @@
 package net.kravuar.components.subjects.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -8,12 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CalculatorTests {
     Calculator calculator = new CalculatorImpl();
 
-    @Test
-    void factorialIsCorrect() {
-        assertEquals(calculator.factorial(0), 1);
-        assertEquals(calculator.factorial(3), 6);
-        assertEquals(calculator.factorial(4), 24);
-        assertEquals(calculator.factorial(7), 5040);
+    @ParameterizedTest(name = "factorial of {0} is {1}")
+    @CsvSource({
+            "0, 1",
+            "3, 6",
+            "4, 24",
+            "7, 5040"
+    })
+    void factorialIsCorrect(int number, int result) {
+        assertEquals(calculator.factorial(number), result);
     }
 
     @Test
